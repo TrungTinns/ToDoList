12d66a4d2181723090d584936ed6f641c7391de4
TrungTinns <trungtin073@gmail.com>
2024-10-27 11:46:09 +0700

    init create todolist using react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button, TextField } from "@mui/material"
+
+function App() {
+  return (
+    <>
+      <p>ToDo App</p>
+      <div>
+        <TextField size="small">
+          <Button variant="contained">
+            Add
+          </Button>
+        </TextField>
+      </div>
+    </>
+  )
+}
+
+export default App
